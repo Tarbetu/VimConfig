e71a1f390cdd3622023b4f8f636b769c47bcf94b
Emrecan Şuşter <emrecansuster@tuta.io>
2022-04-04 09:04:06 +0300

    fixing the ~ thing

--- installation.sh
+++ installation.sh
@@ -8,7 +8,7 @@ fi
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-CONFIG="~/.config/nvim/"
+CONFIG="$HOME/.config/nvim/"
 rm -rf $CONFIG/*
 cp -r * $CONFIG
 rm $CONFIG/installation.sh
